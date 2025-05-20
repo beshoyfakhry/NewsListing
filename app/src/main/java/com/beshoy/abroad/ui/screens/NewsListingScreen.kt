15821204d190485a50f138c4edaff4c5861adbf8
BeshoyFakhry <beshoyfakhry@gmail.com>
2025-05-21 02:03:34 +0300

    fix the recall again when come back from details - fix return back to top

--- app/src/main/java/com/beshoy/abroad/ui/screens/NewsListingScreen.kt
+++ app/src/main/java/com/beshoy/abroad/ui/screens/NewsListingScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,9 +48,14 @@ fun NewsListingScreen(
     newsViewModel: NewsViewModel = hiltViewModel()
 ) {
     val newsState = newsViewModel.newsState.collectAsStateWithLifecycle()
-    LaunchedEffect(isSearch) {
-        newsViewModel.getNews(if (!isSearch) "us" else "")
+
+    LaunchedEffect(Unit) {
+        if (newsViewModel.newsState.value !is ResourceState.Success<*>) {
+            newsViewModel.getNews(if (!isSearch) "us" else "")
+        }
     }
+
+
     ShowNewsList(
         isSearch = isSearch,
         newsState = newsState.value,
@@ -97,8 +104,10 @@ fun ShowNewsList(
 //Comment
                 is ResourceState.Success<*> -> {
                     val articles = (newsState.data as NewsResponse).articles
-
-                    LazyColumn(
+                    val listState = rememberSaveable(saver = LazyListState.Saver) {
+                        LazyListState()
+                    }
+                    LazyColumn(state = listState,
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(8.dp),
